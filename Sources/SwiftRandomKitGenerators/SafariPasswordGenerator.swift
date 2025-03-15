9a98775f5522802e9c151c0b1d84d4c0d7217208
Ibrahim Koteish <ikteish@yahoo.com>
2025-03-15 03:05:12 +0100

    Update Safari pass implementation

--- Sources/SwiftRandomKitGenerators/SafariPasswordGenerator.swift
+++ Sources/SwiftRandomKitGenerators/SafariPasswordGenerator.swift
@@ -5,14 +5,7 @@ public struct SafariPasswordGenerator: RandomGenerator {
 
     public func run<RNG: RandomNumberGenerator>(using rng: inout RNG) -> String {
         // huwKun-1zyjxi-nyxseh
-        let chars =  Always(
-            Array(
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-            )
-        )
-
-        let alphanum = chars.element().map { $0! }
-        let passwordSegment = alphanum.array(6).map(charsToString)
+        let passwordSegment = RandomGenerators.letterOrNumber.array(6).map(charsToString)
         let password = passwordSegment.array(3).map { $0.joined(separator: "-") }
         return password.run(using: &rng)
     }
